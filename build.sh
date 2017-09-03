dff741994ab316fc0ce73182d8c77c0df7f34696
Carsten Elton Sorensen <csoren@gmail.com>
2017-09-03 22:41:20 +0200

    Progress on converted Z80 BIOS code

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ asmz80=asmotor/build/scons/build_gameboy/motorgb
 link=asmotor/build/scons/build_xlink/xlink
 
 $asm6502 -fx -omain.o main.65
-$asmz80 -fx -oloader.o loader.z80
+$asmz80 -mcz -fx -oloader.o loader.z80
 $link -tc128 -oloader.prg main.o loader.o
 
