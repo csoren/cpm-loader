fbd5d3213ab16f9bd5acc7032bb6a254a6ccf901
Carsten Elton Sorensen <csoren@gmail.com>
2017-09-03 11:06:05 +0200

    Uses new xlink .prg support

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ cd ../../..
 
 asm6502=asmotor/build/scons/build_6502/motor6502
 asmz80=asmotor/build/scons/build_gameboy/motorgb
+link=asmotor/build/scons/build_xlink/xlink
 
-$asm6502 -fb -oloader.prg main.asm
+$asm6502 -fx -omain.o main.asm
+$link -tc128 -oloader.prg main.o
+
